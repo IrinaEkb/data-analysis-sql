4e3c0685f6751a5e496bb9cbc1def448693f912c
IrinaEkb <irinafendley@gmail.com>
2026-08-26 15:52:07 -0400

    fix

--- medicare/sql/02_data_cleaning.sql.sql
+++ medicare/sql/02_data_cleaning.sql.sql
@@ -1,16 +1,12 @@
+DROP TABLE IF EXISTS state_summary_clean;
+
 CREATE TABLE state_summary_clean AS
 SELECT
     UPPER(TRIM(state)) AS state,
-
-    CAST(NULLIF(TRIM(total_payments), '') AS FLOAT) AS total_payments,
-    CAST(NULLIF(TRIM(total_services), '') AS FLOAT) AS total_services,
-    CAST(NULLIF(TRIM(total_providers), '') AS FLOAT) AS total_providers,
-
-    CASE
-        WHEN avg_markup_ratio IS NULL THEN NULL
-        ELSE CAST(avg_markup_ratio AS FLOAT)
-        END AS avg_markup_ratio
-
+    total_payments,
+    total_services,
+    total_providers,
+    avg_markup_ratio
 FROM state_summary
 WHERE state IS NOT NULL
   AND UPPER(TRIM(state)) IN (
@@ -18,6 +14,8 @@ WHERE state IS NOT NULL
                              'HI','ID','IL','IN','IA','KS','KY','LA','ME','MD',
                              'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ',
                              'NM','NY','NC','ND','OH','OK','OR','PA','RI','SC',
-                             'SD','TN','TX','UT','VT','VA','WA','WV','WI','WY',
-                             'DC'
+                             'SD','TN','TX','UT','VT','VA','WA','WV','WI','WY','DC'
     );
+
+SELECT COUNT(*) FROM state_summary_clean;
+-- 50 states + DC = 51 rows, no missing values, no duplicates, no zero values, no leading/trailing spaces, no inconsistent state format, no negative values
